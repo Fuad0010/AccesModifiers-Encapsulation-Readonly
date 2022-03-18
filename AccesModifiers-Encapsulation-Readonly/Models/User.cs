e74e3b954478844a79a7c33a83b8297c09e3fea5
Fuad0010 <fuadft@code.edu.az>
2022-03-18 16:23:20 +0400

    Add project files.

---
+++ AccesModifiers-Encapsulation-Readonly/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesModifiers_Encapsulation_Readonly.Models
+{
+    internal class User
+    {
+        public string Name;
+        public int Age;
+        public string Password;
+
+        public User(string name, int age, string password)
+        {
+            Name = name;
+            Age = age;
+            Password = password;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"----------------------------\n"+
+                              $"Name: {Name}\n" +
+                              $"Age: {Age}\n" +
+                              $"Password: {Password}");
+        }
+
+        
+    }
+    
+
+
+
+}
---
+++ AccesModifiers-Encapsulation-Readonly/Program.cs
@@ -0,0 +1,74 @@
+﻿using AccesModifiers_Encapsulation_Readonly.Models;
+using System;
+
+namespace AccesModifiers_Encapsulation_Readonly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count1 = 1;
+            
+            User[] user = new User[count1];
+            for (int i = 0; i < count1; i++)
+            {
+
+                string name = GetInputWord("Please enter the name: ",1,20);
+
+                int age = GetInputInt("Please enter the age: ", 1, 100);
+
+                string password = GetInputWord("Password: ", 8, 100);
+
+                user[i] = new User(name, age, password)
+                {
+                    Name = name,
+                    Age = age,
+                    Password = password
+                };
+
+            }
+            foreach (var item in user)
+            {
+                item.GetInfo();
+            }        
+        
+        
+        }
+            static int GetInputInt(string name, int minValue, int maxValue)
+        {
+
+
+            int input;
+
+            do
+            {
+                Console.Write(name);
+                input = Convert.ToInt32(Console.ReadLine());
+
+            } while (input < minValue || input > maxValue);
+            return input;
+        }
+
+        static string GetInputWord(string name, int minValue, int maxValue)
+        {
+
+
+            string input;
+
+            do
+            {
+                Console.Write(name);
+                input = Console.ReadLine();
+
+            } while (input.Length < minValue || input.Length > maxValue);
+            return input;
+        }
+
+
+
+
+
+
+
+    }
+}
